92def8560a6e9b75c8378b5953111e9e97cf8be6
Behnam1382 <behnam.ghasemi082@gmail.com>
2025-09-04 10:28:12 +0330

    fix: change vite config for build package

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,39 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
-})
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "SmartDialog",
+      formats: ["es", "cjs"],
+      fileName: (format) =>
+        format === "es" ? "index.esm.js" : "index.cjs",
+    },
+    rollupOptions: {
+      external: [
+        "react",
+        "react-dom",
+        "@mui/material",
+        "@mui/icons-material",
+        "@emotion/react",
+        "@emotion/styled",
+        "styled-components"
+      ],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          "@mui/material": "MaterialUI",
+          "@mui/icons-material": "MaterialUIIcons",
+          "@emotion/react": "emotionReact",
+          "@emotion/styled": "emotionStyled",
+          "styled-components": "styled"
+        }
+      }
+    }
+  }
+});
